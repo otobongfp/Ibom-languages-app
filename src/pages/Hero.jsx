561ab7e42d78b790ff894c5e83591d2bf3b4ee70
unknown <otobongfp@gmail.com>
2024-01-23 20:30:08 +0100

    minor update

--- src/pages/Hero.jsx
+++ src/pages/Hero.jsx
@@ -126,7 +126,7 @@ export default function Hero() {
           />
         </div>
         <div className="mx-auto max-w-2xl py-32 sm:py-48 lg:py-56">
-          <div className="hidden sm:mb-8 sm:flex sm:justify-center">
+          {/* <div className="hidden sm:mb-8 sm:flex sm:justify-center">
             <div className="relative rounded-full px-3 py-1 text-sm leading-6 text-gray-600 ring-1 ring-gray-900/10 hover:ring-gray-900/20">
               What we are working on.{" "}
               <a href="#" className="font-semibold text-indigo-600">
@@ -134,7 +134,7 @@ export default function Hero() {
                 Read more <span aria-hidden="true">&rarr;</span>
               </a>
             </div>
-          </div>
+          </div> */}
           <div className="text-center">
             <h1 className="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">
               Ibom Languages Project
